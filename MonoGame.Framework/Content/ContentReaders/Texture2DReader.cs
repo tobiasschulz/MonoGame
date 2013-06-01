fb9a61ba86a3293d97c81cbfa0328ae82a0dd8ab
Ethan Lee <flibitijibibo@gmail.com>
2013-06-01 14:00:09 -0400

    Only generate mipmaps if the texdata has them. Yeah... I know.

--- MonoGame.Framework/Content/ContentReaders/Texture2DReader.cs
+++ MonoGame.Framework/Content/ContentReaders/Texture2DReader.cs
@@ -242,9 +242,10 @@ namespace Microsoft.Xna.Framework.Content
 			}
 
 			// FIXME: This may apply to more compressed formats!
-			if (	convertedFormat == SurfaceFormat.Dxt1 ||
-				convertedFormat == SurfaceFormat.Dxt3 ||
-				convertedFormat == SurfaceFormat.Dxt5	)
+			if (	levelCount > 1 &&
+				(	convertedFormat == SurfaceFormat.Dxt1 ||
+					convertedFormat == SurfaceFormat.Dxt3 ||
+					convertedFormat == SurfaceFormat.Dxt5	)	)
 			{
 				texture.GenerateMipmaps();
 			}
