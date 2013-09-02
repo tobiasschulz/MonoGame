a62b766fb6715e247621e81bbae696419c7446d3
Ethan Lee <flibitijibibo@gmail.com>
2013-09-02 09:20:43 -0400

    Remove unused using statement.

--- MonoGame.Framework/Game.cs
+++ MonoGame.Framework/Game.cs
@@ -85,7 +85,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
-using Microsoft.Xna.Framework.GamerServices;
 
 
 namespace Microsoft.Xna.Framework
