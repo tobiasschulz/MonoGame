c85ee7e46ffad0bf6355f1e655efc756e89750a5
Ethan Lee <flibitijibibo@gmail.com>
2014-02-16 17:31:48 -0500

    So this file exists apparently!

--- MonoGame.Framework/Graphics/OcclusionQuery.cs
+++ MonoGame.Framework/Graphics/OcclusionQuery.cs
@@ -1,86 +1,67 @@
-using System;
-using System.Runtime.InteropServices;
-
-
-#if OPENGL
 using OpenTK.Graphics.OpenGL;
-#endif
 
 namespace Microsoft.Xna.Framework.Graphics
 {
-	public class OcclusionQuery : GraphicsResource
-	{
-#if OPENGL
-		private uint glQueryId;
-#endif
+    public class OcclusionQuery : GraphicsResource
+    {
+        private uint glQueryId;
 
-		public OcclusionQuery (GraphicsDevice graphicsDevice)
-		{
-			this.GraphicsDevice = graphicsDevice;
-#if OPENGL
-			GL.GenQueries (1, out glQueryId);
+        public OcclusionQuery(GraphicsDevice graphicsDevice)
+        {
+            this.GraphicsDevice = graphicsDevice;
+            GL.GenQueries(1, out glQueryId);
             GraphicsExtensions.CheckGLError();
-#elif DIRECTX
-#endif
-		}
+        }
 
-		public void Begin ()
-		{
-#if OPENGL
-			GL.BeginQuery (QueryTarget.SamplesPassed, glQueryId);
+        public void Begin()
+        {
+            GL.BeginQuery(QueryTarget.SamplesPassed, glQueryId);
             GraphicsExtensions.CheckGLError();
-#elif DIRECTX
-#endif
+        }
 
-		}
-
-		public void End ()
-		{
-#if OPENGL
-			GL.EndQuery (QueryTarget.SamplesPassed);
+        public void End()
+        {
+            GL.EndQuery(QueryTarget.SamplesPassed);
             GraphicsExtensions.CheckGLError();
-#elif DIRECTX
-#endif
+        }
 
-		}
-
-		protected override void Dispose(bool disposing)
-		{
+        protected override void Dispose(bool disposing)
+        {
             if (!IsDisposed)
             {
-#if OPENGL
                 GraphicsDevice.AddDisposeAction(() =>
-                    {
-                        GL.DeleteQueries(1, ref glQueryId);
-                        GraphicsExtensions.CheckGLError();
-                    });
-#elif DIRECTX
-#endif
+                {
+                    GL.DeleteQueries(1, ref glQueryId);
+                    GraphicsExtensions.CheckGLError();
+                });
             }
             base.Dispose(disposing);
-		}
+        }
 
-		public bool IsComplete {
-			get {
-				int[] resultReady = {0};
-#if OPENGL
+        public bool IsComplete
+        {
+            get
+            {
+                int[] resultReady = {0};
                 GL.GetQueryObject(glQueryId, GetQueryObjectParam.QueryResultAvailable, resultReady);
                 GraphicsExtensions.CheckGLError();
-#elif DIRECTX                
-#endif
-				return resultReady[0] != 0;
-			}
-		}
-		public int PixelCount {
-			get {
-				int[] result = {0};
-#if OPENGL
-                GL.GetQueryObject(glQueryId, GetQueryObjectParam.QueryResultAvailable, result);
+                return resultReady[0] != 0;
+            }
+        }
+
+        public int PixelCount
+        {
+            get
+            {
+                int[] result = {0};
+                GL.GetQueryObject(
+                    glQueryId,
+                    GetQueryObjectParam.QueryResultAvailable,
+                    result
+                );
                 GraphicsExtensions.CheckGLError();
-#elif DIRECTX             
-#endif
                 return result[0];
-			}
+            }
         }
     }
 }
