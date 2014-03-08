a0c30a02a815eb24185ec219e6563235bca1b701
Ethan Lee <flibitijibibo@gmail.com>
2014-03-08 11:45:22 -0500

    EG2 buildfix

--- MonoGame.Framework/Graphics/DeviceNotResetException.cs
+++ MonoGame.Framework/Graphics/DeviceNotResetException.cs
@@ -3,7 +3,7 @@ using System;
 namespace Microsoft.Xna.Framework
 {
     [Serializable]
-    public class DeviceLostException : Exception
+    public class DeviceNotResetException : Exception
     {
         // TODO: How exactly does this behave? Also, lol, DirectX.
     }
