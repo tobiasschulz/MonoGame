a07304dfe34ae7a5fbed438fbf087b770b1e964c
Jesse Litton <git@eternaldusk.com>
2014-03-15 06:34:26 -0500

    FNA License Cleanup for Graphics/Vertices.

--- MonoGame.Framework/Graphics/Vertices/BufferUsage.cs
+++ MonoGame.Framework/Graphics/Vertices/BufferUsage.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿#region License
+/* FNA - XNA4 Reimplementation for Desktop Platforms
+ * Copyright 2009-2014 Ethan Lee and the MonoGame Team
+ *
+ * Released under the Microsoft Public License.
+ * See LICENSE for details.
+ */
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
