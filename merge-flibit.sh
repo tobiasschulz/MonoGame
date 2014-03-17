485218d11a8aa64bb71469ad354e8cb9815cf926
Tobias Schulz <tobias@freehal.net>
2014-03-17 23:44:32 +0100

    merge script

---
+++ merge-flibit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# git remote add flibit https://github.com/flibitijibibo/MonoGame.git
+git pull
+git checkout -f monogame-sdl2
+git fetch flibit
+git reset --hard flibit/monogame-sdl2
+git push origin monogame-sdl2  -f
+
+git pull
+git checkout -f mgsdl2-glshader
+git pull
+git merge monogame-sdl2 && git commit -a && git push origin monogame-sdl2
