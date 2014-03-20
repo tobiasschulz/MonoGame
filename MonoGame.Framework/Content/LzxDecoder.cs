d78ed9f7af93d28b4dba831fbc23cb3282dfc9c8
Ethan Lee <flibitijibibo@gmail.com>
2014-03-19 21:47:02 -0400

    LzxDecoder license cleanup

--- MonoGame.Framework/Content/LzxDecoder.cs
+++ MonoGame.Framework/Content/LzxDecoder.cs
@@ -1,45 +1,17 @@
-#region HEADER
-/* This file was derived from libmspack
- * (C) 2003-2004 Stuart Caie.
- * (C) 2011 Ali Scissons.
+#region License
+/* FNA - XNA4 Reimplementation for Desktop Platforms
+ * Copyright 2009-2014 Ethan Lee and the MonoGame Team
  *
- * The LZX method was created by Jonathan Forbes and Tomi Poutanen, adapted
- * by Microsoft Corporation.
+ * Released under the Microsoft Public License.
+ * See LICENSE for details.
+ */
+
+/* LzxDecoder.cs - C# port of libmsport's lzxd.c
+ * Copyright 2003-2004 Stuart Caie
+ * Copyright 2011 Ali Scissons
  *
- * This source file is Dual licensed; meaning the end-user of this source file
- * may redistribute/modify it under the LGPL 2.1 or MS-PL licenses.
- */ 
-#region LGPL License
-/* GNU LESSER GENERAL PUBLIC LICENSE version 2.1
- * LzxDecoder is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License (LGPL) version 2.1 
- */
-#endregion
-#region MS-PL License
-/* 
- * MICROSOFT PUBLIC LICENSE
- * This source code is subject to the terms of the Microsoft Public License (Ms-PL). 
- *  
- * Redistribution and use in source and binary forms, with or without modification, 
- * is permitted provided that redistributions of the source code retain the above 
- * copyright notices and this file header. 
- *  
- * Additional copyright notices should be appended to the list above. 
- * 
- * For details, see <http://www.opensource.org/licenses/ms-pl.html>. 
- */
-#endregion
-/*
- * This derived work is recognized by Stuart Caie and is authorized to adapt
- * any changes made to lzxd.c in his libmspack library and will still retain
- * this dual licensing scheme. Big thanks to Stuart Caie!
- * 
- * DETAILS
- * This file is a pure C# port of the lzxd.c file from libmspack, with minor
- * changes towards the decompression of XNB files. The original decompression
- * software of LZX encoded data was written by Suart Caie in his
- * libmspack/cabextract projects, which can be located at 
- * http://http://www.cabextract.org.uk/
+ * Released under a dual MSPL/LGPL license.
+ * See lzxdecoder.LICENSE for details.
  */
 #endregion
 
